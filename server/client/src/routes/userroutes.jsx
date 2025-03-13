112cae4b14d53c17f3568fc33ba0be7e05461fe7
pycogram <kaluifesinachi@yahoo.com>
2025-03-13 02:40:33 +0100

    adjusted reg page code

--- server/client/src/routes/userroutes.jsx
+++ server/client/src/routes/userroutes.jsx
@@ -4,7 +4,6 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const UserRoutes = () => {
     const {user} = useContext(UserContext);
-    //console.log(1234, user);
     return ( 
         user.email ? <Outlet /> : <Navigate to={'/login'} />
     );
